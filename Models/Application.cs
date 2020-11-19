99f6fc96b14f49658fa0a1b2c0ac1e4eac9f97ef
zc0rp10 <zcorpio@gmail.com>
2020-11-19 17:30:27 +0100

    Added application CRUD pages & models

---
+++ Models/Application.cs
@@ -0,0 +1,10 @@
+namespace BlazorBearTrac.Models
+{
+    public class Application
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Language { get; set; }
+    }
+}
+
