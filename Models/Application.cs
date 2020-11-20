908a2f0d38cf65095e64c397723ace78925afab7
zc0rp10 <zcorpio@gmail.com>
2020-11-20 22:20:29 +0100

    Added Description to Applications

--- Models/Application.cs
+++ Models/Application.cs
@@ -5,6 +5,7 @@ namespace BlazorBearTrac.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
+        public string Description { get; set; }
         public string Language { get; set; }
     }
 
